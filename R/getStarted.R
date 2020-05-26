145019f09a357564e267c24d91cd2d745a2ea5af
Lei <lei.fang.87@gmail.com>
2020-05-26 18:44:53 +0100

    update

--- R/getStarted.R
+++ R/getStarted.R
@@ -1,4 +1,6 @@
 X50 <- readRDS("~/Projects/R/aiquant/aiqtt_data/X50.rds")
 ret50 <- readRDS("~/Projects/R/aiquant/aiqtt_data/ret50.rds")
+source('~/Projects/R/aiquant/R/autogarch.R')
+library(xts)
 fitX50 = garchAuto(ret50, cores=2, trace=TRUE)
 plot(fitX50)
